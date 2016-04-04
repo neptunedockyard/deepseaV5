3af3ea5cb635544d998fab1da7a9696a832bae09
atreyu <miharu1984@yahoo.com>
2016-04-04 00:17:43 -0700

    added some demo code and textures

--- core/src/com/deepsea/Game.java
+++ core/src/com/deepsea/Game.java
@@ -2,28 +2,40 @@ package com.deepsea;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
 import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.FirstPersonCameraController;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 
 public class Game extends ApplicationAdapter {
 
 	//controllers
 	public PerspectiveCamera cam;
 	public FirstPersonCameraController camCon;
+	public ModelBuilder modelBuilder;
+	public Model model;
+	public ModelBatch modelBatch;
+	public Array<ModelInstance> instances = new Array<ModelInstance>();
+	public ModelInstance modInstance;
 	
 	//environment
 	public Environment lights;
 	
 	
 	//game vars
-	public static Vector3 playerStartpos;
-	public static Vector3 playerStartlook;
+	
 	
 	@Override
 	public void create () {
@@ -31,10 +43,8 @@ public class Game extends ApplicationAdapter {
 		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		cam.near = 0.5f;
 		cam.far = 100f;
-		playerStartpos = new Vector3(0, 0, 0);
-		playerStartlook = new Vector3(10, 0, 10);
-		cam.position.set(playerStartpos);
-		cam.lookAt(playerStartlook);
+		cam.position.set(0,0,3);
+		cam.lookAt(0,0,0);
 		cam.update();
 		
 		//create the camera controller and input
@@ -46,18 +56,28 @@ public class Game extends ApplicationAdapter {
 		lights.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1.f));
 		lights.add(new DirectionalLight().set(1, 1, 1, 0, -1, 0));
 		
-		MathUtils.random.setSeed(0);
+		//create models
+		modelBuilder = new ModelBuilder();
+		model = modelBuilder.createSphere(2f, 2f, 2f, 20, 20, new Material(ColorAttribute.createDiffuse(Color.BLUE)), Usage.Position | Usage.Normal | Usage.TextureCoordinates);
 		
+		modInstance = new ModelInstance(model, 10, 10, 10);
+		modelBatch = new ModelBatch();
 	}
 
 	@Override
 	public void render () {
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		//clear screen
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 		
+		cam.rotateAround(Vector3.Zero, new Vector3(0,1,0),1f);
 		cam.update();
-		camCon.update();
+		//camCon.update();
+		
+		modelBatch.begin(cam);
+		modelBatch.render(modInstance, lights);
+		modelBatch.end();
 	}
 
 	@Override
@@ -65,6 +85,12 @@ public class Game extends ApplicationAdapter {
 		cam.viewportWidth = width;
 		cam.viewportHeight = height;
 		cam.update();
+	}
+	
+	@Override
+	public void dispose() {
+		model.dispose();
+		modelBatch.dispose();
 	}
 
 }
